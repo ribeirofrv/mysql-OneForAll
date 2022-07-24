545538edb87159b5ccf52b59c06ab20f51068197
Fran Ribeiro <ribeirofrv@gmail.com>
2022-07-24 15:00:27 -0300

    feat: Create subscription plans table.

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,12 @@
 DROP DATABASE IF EXISTS SpotifyClone;
 
 CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE
+  budget_plans(
+    plan_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    plan_name VARCHAR(50) NOT NULL,
+    price INT NOT NULL
+  ) engine = InnoDB;
