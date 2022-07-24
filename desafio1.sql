de4174e3fc7d64b787f516458b6cf528ef2c4ba0
Fran Ribeiro <ribeirofrv@gmail.com>
2022-07-24 15:03:49 -0300

    feat: Create users table.

--- desafio1.sql
+++ desafio1.sql
@@ -10,3 +10,13 @@ CREATE TABLE
     plan_name VARCHAR(50) NOT NULL,
     price INT NOT NULL
   ) engine = InnoDB;
+
+CREATE TABLE
+  users(
+    user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(50) NOT NULL,
+    age INT NOT NULL,
+    signature_date DATE NOT NULL,
+    budget_plans_id INT NOT NULL,
+    FOREIGN KEY (budget_plans_id) REFERENCES budget_plans(plan_id)
+  ) engine = InnoDB;
