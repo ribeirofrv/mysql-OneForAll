6bcc6c51d134b0b5bd678374cfaf682419b46270
Fran Ribeiro <ribeirofrv@gmail.com>
2022-07-25 21:20:22 -0300

    fix:  that displays information about the company's billing. plan table name

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,8 @@ SELECT
   FORMAT(MIN(plan.price), 2) AS faturamento_minimo,
   FORMAT(MAX(plan.price), 2) AS faturamento_maximo,
   FORMAT(AVG(plan.price), 2) AS faturamento_medio,
-  FORMAT(SUM(plan.price) , 2) AS faturamento_total
+  FORMAT(SUM(plan.price), 2) AS faturamento_total
 FROM
-  budget_plans AS plan
-  INNER JOIN users ON users.budget_plans_id = plan.plan_id;
+  signature_plans AS plan
+  INNER JOIN users ON users.signature_plans_id = plan.plan_id;
+
